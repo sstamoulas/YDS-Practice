77f8e1d859735ac58936e3c381256663fc9610b6
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2020-02-09 15:23:58 +0300

    Updates

---
+++ precache-manifest.164a3dcaca031013310ea3f65b986759.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ac856745bbbc01ce148d111db8b0c8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9de841c756fbf0b4a635",
+    "url": "/static/css/main.06dd9884.chunk.css"
+  },
+  {
+    "revision": "35e3c78388aaee13b0a0",
+    "url": "/static/js/2.6c51a41f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.6c51a41f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9de841c756fbf0b4a635",
+    "url": "/static/js/main.8d1126d8.chunk.js"
+  },
+  {
+    "revision": "29121edf3e520e373970",
+    "url": "/static/js/runtime-main.1c41147e.js"
+  }
+]);
