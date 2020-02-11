b86043c7394629ccbdfbfd1fbd286fdae0473ce4
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2020-02-11 22:47:49 +0300

    Added color transition to text and background

--- src/components/question/question.component.jsx
+++ src/components/question/question.component.jsx
@@ -16,13 +16,33 @@ class Question extends Component {
       randomSectionIndex: randomSectionIndex,
       randomQuestionIndex: randomQuestionIndex,
       showSolution: false,
+      textColor: 'black',
     };
+  }
+
+  generateRandomNumber = (length) => {
+    return parseInt(Math.random() * (length));
+  }
+  
+  generateRandomColor = () => {
+    let randomColor = this.generateRandomNumber(0xFFFFFF<<0).toString(16);
+    let length = randomColor.length;
+    for(let i = 0; i < length; i++) {
+      if(length < 6) {
+        randomColor = 0 + randomColor;
+        length++;
+      }
+    }
+
+    return ('#' + randomColor);
   }
 
   handleClick = () => {
     let { questions } = this.state.data[this.state.randomSectionIndex];
     let randomSectionIndex = this.state.randomSectionIndex;
-    let randomQuestionIndex = this.state.randomQuestionIndex;;
+    let randomQuestionIndex = this.state.randomQuestionIndex;
+    let bodyTag = document.getElementsByTagName("body")[0];
+    let randomColor = this.generateRandomColor();
 
     if((randomSectionIndex + 1) >= this.state.data.length && (randomQuestionIndex + 1) >= questions.length) {
       randomQuestionIndex = 0;
@@ -37,10 +57,21 @@ class Question extends Component {
     }
 
     this.setState({ 
-      randomSectionIndex: randomSectionIndex,
-      randomQuestionIndex: randomQuestionIndex,
-      showSolution: false,
-     });
+      textColor: 'white',
+    });
+
+    bodyTag.style.backgroundColor = 'white';
+
+    setTimeout(() => {
+      this.setState({ 
+        randomSectionIndex: randomSectionIndex,
+        randomQuestionIndex: randomQuestionIndex,
+        showSolution: false,
+        textColor: 'black',
+      });
+
+      bodyTag.style.backgroundColor = randomColor;
+    }, 500);
   }
 
   showSolution = () => {
@@ -56,22 +87,22 @@ class Question extends Component {
 
     return (
       <div className="container">
-        <h3>{number}) Directions: {directions}</h3>
+        <h3 className="text-transition" style={{color: this.state.textColor}}>{number}) Directions: {directions}</h3>
         {
           isDialogue ?
-            <p dangerouslySetInnerHTML={{__html: question.replace(/\n/g, '<br/>')}}></p>
+            <p className="text-transition" style={{color: this.state.textColor}} dangerouslySetInnerHTML={{__html: question.replace(/\n/g, '<br/>')}}></p>
           :
           text !== undefined ?
-          <div>
+          <div className="text-transition" style={{color: this.state.textColor}}>
             <p>{text}</p>
             <h3>{question}</h3>
           </div>
           : 
-          <h3>{question}</h3>
+          <h3 className="text-transition" style={{color: this.state.textColor}}>{question}</h3>
         }
         {
           choices.map((choice, index) => (
-            <li key={index}>{choice}</li>
+            <li key={index} className="text-transition" style={{color: this.state.textColor}}>{choice}</li>
           ))
         }
         <span className={this.state.showSolution ? 'active solution' : 'not-active solution'} >Answer: {choices[answer]}</span>
