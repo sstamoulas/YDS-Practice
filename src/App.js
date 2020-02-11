2e13c3b7addb3df4bdade4dc41c1a01b4c5be0c4
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2020-02-11 13:57:25 +0300

    Added history and page tracking for GA

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
 
 import HomePage from './pages/home-page.component';
 
 import './App.css';
 
+const history = createBrowserHistory();
+
 ReactGA.initialize('UA-158180736-1');
+
+history.listen((location) => {
+    ReactGA.set({ page: location.pathname });
+    ReactGA.pageview(location.pathname)
+  }
+);
 
 function App() {
   return (
