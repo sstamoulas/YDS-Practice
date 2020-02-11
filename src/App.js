f6ba20ef4445a2b293dd8473c261bef28fa615a2
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2020-02-11 13:30:16 +0300

    Added react-ga for google analytics tracking

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import ReactGA from 'react-ga';
 
 import HomePage from './pages/home-page.component';
 
 import './App.css';
+
+function initializeReactGA() {
+    ReactGA.initialize('UA-158180736-1');
+    ReactGA.pageview('/homepage');
+}
 
 function App() {
   return (
